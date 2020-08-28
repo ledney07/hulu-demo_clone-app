1a7db920f6be42c58783920d2720abd57e3ee492
ledney07 <ledney21@gmail.com>
2020-08-27 21:26:39 -0400

    Updates

---
+++ precache-manifest.a395adb8e30f515d8c3f5c3837347534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b1858eaebb63dd6781138d988da7e6",
+    "url": "/hulu-demo_clone-app/index.html"
+  },
+  {
+    "revision": "5b8d9f26b09fc14091b2",
+    "url": "/hulu-demo_clone-app/static/css/main.863ab15e.chunk.css"
+  },
+  {
+    "revision": "25ede299dc1a62099c09",
+    "url": "/hulu-demo_clone-app/static/js/2.18fc9d30.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/hulu-demo_clone-app/static/js/2.18fc9d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8d9f26b09fc14091b2",
+    "url": "/hulu-demo_clone-app/static/js/main.1f2325a6.chunk.js"
+  },
+  {
+    "revision": "58fcaa0139b78237179c",
+    "url": "/hulu-demo_clone-app/static/js/runtime-main.de6931ab.js"
+  }
+]);
